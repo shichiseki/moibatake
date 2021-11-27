b36c7fe474b540d9e0f9993d137118d4f84ce975
Suguru677 <s.m.yt.939@outlook.jp>
2021-11-27 11:24:50 +0900

    fix PORT

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use('/img', express.static(__dirname + '/dist/img/'));
 app.use('/css', express.static(__dirname + '/dist/css/'));
 app.use('/js', express.static(__dirname + '/dist/js/'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
 
-app.listen(5000, () => console.log('Example app listening on port 5000!'))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 app.get('/api/hello', (req, res) => {
     res.send('hello i am express')
 })
